5c80d023b653741a921424537306c7e5b3f5ba99
Pontus Asp <pontus.asp@gmail.com>
2018-11-27 18:15:35 +0100

    Bound onClick to java code

--- app/src/main/java/com/pefjs/gametoolspefjs/MainActivity.java
+++ app/src/main/java/com/pefjs/gametoolspefjs/MainActivity.java
@@ -2,6 +2,7 @@ package com.pefjs.gametoolspefjs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttonClick(View view) {
+        switch(view.getId()) {
+            case R.id.button_play:
 
+                break;
+            case R.id.button_host:
+
+                break;
+            case R.id.button_join:
+
+                break;
+        }
     }
 }
