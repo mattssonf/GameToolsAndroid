2d645cb207b231c2b95f7ce7ff3990f4dae1d005
sigge94 <sigge.94@live.se>
2018-11-28 14:45:41 +0100

    Namebox added

--- app/src/main/java/com/pefjs/gametoolspefjs/NameScreenActivity.java
+++ app/src/main/java/com/pefjs/gametoolspefjs/NameScreenActivity.java
@@ -2,6 +2,8 @@ package com.pefjs.gametoolspefjs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class NameScreenActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class NameScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_screen);
+    }
+
+    public void saveName(View view){
+        EditText editText = (EditText) findViewById(R.id.chooseNameText);
+
+
     }
 }
