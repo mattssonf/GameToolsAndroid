0043029dfdd29c5a7c766df166477618ba7d53a2
Pontus Asp <pontus.asp@gmail.com>
2018-11-29 22:14:27 +0100

    Added Join screen

--- app/src/main/java/com/pefjs/gametoolspefjs/MainActivity.java
+++ app/src/main/java/com/pefjs/gametoolspefjs/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pefjs.gametoolspefjs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
                 break;
             case R.id.button_join:
-
+                Intent intentJoin = new Intent(this, JoinActivity.class);
+                startActivity(intentJoin);
                 break;
         }
     }
