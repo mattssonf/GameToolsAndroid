bda8c907f001c150f8c59fcd926e2283ca1e4e06
Pontus Asp <pontus.asp@gmail.com>
2018-11-30 00:32:32 +0100

    Added Room Activity

---
+++ app/src/main/java/com/pefjs/gametoolspefjs/RoomActivity.java
@@ -0,0 +1,24 @@
+package com.pefjs.gametoolspefjs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(getActivity());
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+        }
+
+    }
+}
