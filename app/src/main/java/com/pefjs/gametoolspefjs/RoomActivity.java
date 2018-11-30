d90bbb2f5498da26fce6ceb86db3e5d627c130d7
Pontus Asp <pontus.asp@gmail.com>
2018-11-30 13:17:35 +0100

    Made Room take extras from Name

--- app/src/main/java/com/pefjs/gametoolspefjs/RoomActivity.java
+++ app/src/main/java/com/pefjs/gametoolspefjs/RoomActivity.java
@@ -1,7 +1,9 @@
 package com.pefjs.gametoolspefjs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RoomActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class RoomActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room);
 
+        Intent intent = getIntent();
+        String username = intent.getStringExtra(NameActivity.NAME);
+
+        String room = intent.getStringExtra(JoinActivity.ROOM);
+
+        if(room == null) {
+            // TODO: CREATE ROOM
+
+            room = "9857";
+        }
+
+        ((TextView) findViewById(R.id.text_username)).setText(username);
+        ((TextView) findViewById(R.id.text_room)).append(room);
     }
 }
